69a46914e46f00438cdb87cbf9019328125927f6
Louis Philippe Querel <lquerel@gmail.com>
2014-09-29 16:55:29 -0400

    Completed extraction from git

--- paper-replication/scripts/git_line_blame/git_line_change_origin_blame.py
+++ paper-replication/scripts/git_line_blame/git_line_change_origin_blame.py
@@ -4,7 +4,7 @@ from subprocess import check_output
 
 __author__ = 'lquerel'
 
-git_path = '/home/lquerel/Documents/MASTERS/LOG6307/6307 Assignment/django'
+git_path = '/home/lquerel/django'
 diff_history_size = 0
 
 
@@ -31,6 +31,11 @@ class Postgres:
         return commits
         # self.db.commit()
 
+    def create_commit_link(self, originating_commit, replaced_by_commit, line_count):
+        cursor = self.db.cursor()
+        cursor.execute("""INSERT INTO GIT_COMMIT_BLAME (ORIGIN, REPLACED_BY, LINE_COUNT) VALUES ('%s','%s',%s)""" %(originating_commit, replaced_by_commit, line_count))
+        self.db.commit()
+
 
 def obtain_git_diff(git_path, commit_id):
     """
@@ -45,7 +50,7 @@ def obtain_git_diff(git_path, commit_id):
 
 
 def obtain_git_blame(git_path, parent_commit_id, file_path, start, end):
-    command = "cd '%s'; git checkout -q master;  git checkout -q %s; git blame -L %s,%s -l -s %s" % (git_path, parent_commit_id, start, end, file_path)
+    command = "cd '%s'; git checkout -q master; git clean -f -d;  git checkout -q %s; git blame -L %s,%s -l -s %s" % (git_path, parent_commit_id, start, end, file_path)
     return check_output(command, shell=True)
     # (\w\d)+\s+(\d+)\)(.*)
 
@@ -58,29 +63,33 @@ def process_revision(revision, parent_commit_id):
 
     ## diff_metrics eg. [('196', '7', '7')] [('a', 'b', 'c')]
     #print "%s %s" % (type(file_diff), file_diff)
+    #print file_diff
 
-    multi = re.findall("@@\s*-(\d+),(\d+)\s*\+\d+,(\d+)", file_diff)
+    #diff_parts = re.findall("(@@.*\n(?:[+-].*\n)+)()", file_diff)
+    #print diff_parts
+
+    #for diff_part in diff_parts:
+    #multi = re.findall("@@\s*-(\d+),(\d+)\s*\+\d+,(\d+)", file_diff)
+
+    multi = re.findall("@@\s*-(\d+)(?:,)?(\d+|)", file_diff)
 
     if len(multi) > 0:
-        diff_metrics = multi[0]
-        change_line_start = int(diff_metrics[0])+diff_history_size
-        change_line_end = int(diff_metrics[0])+int(diff_metrics[1])-diff_history_size-1
+        start = multi[0]
+        end = multi[len(multi)-1]
+        change_line_start = int(start[0])-diff_history_size
+        delta = (int(end[1])+diff_history_size-1) if end[1] else diff_history_size
+        change_line_end = int(end[0])+delta
+
     else:
-        diff_metrics = re.findall("@@\s*-(\d+),{0,1}\d*\s*\+(\d+)", file_diff)
-        if len(diff_metrics) > 0:
-            change_line_start = int(diff_metrics[0][0])+diff_history_size
-            change_line_end = int(diff_metrics[0][1])+1
-        else:
-            print "Possible failure: parent commitid %s revision diff %s" % (parent_commit_id, file_diff)
-            return {}
+        print "Possible failure: parent commitid %s revision diff %s" % (parent_commit_id, file_diff)
+        return {}
 
 
 
     lines_removed = re.findall("\n-(.*)", file_diff)
     number_of_lines_removed = len(lines_removed)
 
-
-    print "\nfile: %s \nstart line: %s\nend line: %s\nremoved: %s" % (file_path, change_line_start, change_line_end, lines_removed)
+    #print "\nfile: %s \nstart line: %s\nend line: %s\nremoved: %s" % (file_path, change_line_start, change_line_end, lines_removed)
     print "file: %s - lines removed: %s" % (file_path, len(lines_removed))
     git_blame = obtain_git_blame(git_path, parent_commit_id, file_path, change_line_start, change_line_end).split("\n")
 
@@ -105,6 +114,8 @@ def process_revision(revision, parent_commit_id):
                     commit_origin_of_removed_lines[line_commit_id] = 1
                 index_of_lines_removed += 1
 
+    if index_of_lines_removed != number_of_lines_removed:
+        print "$$$$$$$$$$$$$$$$$$$$ MISMATCHED $$$$$$$$$$$$$$$$$$$$"
 
     return commit_origin_of_removed_lines
 
@@ -119,13 +130,16 @@ def process_commit(commit_id, parent_commit_id):
     commit_origin_of_removed_lines = {}
     # With the list of all file revisions from the commit diff we need to isolate it to each file revision
     for revision in revisions:
-        parsed_revision = re.findall("a/(.+)\sb(.+\n){4}(@@\s+.+\s+@@.+(.*\n)*)*", revision)
+        parsed_revision = re.findall("a/(.+)\sb(.+\n){3,4}(@@\s+.+\s+@@.+(.*\n)*)*", revision)
         if not parsed_revision:
-            print "This should have been a revision, but was not flagged as one: %s" % parsed_revision
+            print "This should have been a revision, but was not flagged as one: %s" % revision
             # It seems that our revision is not actually a revision. We will skip it then
             continue
 
-        commit_id_and_number_of_lines_affected = process_revision(parsed_revision, parent_commit_id)
+        if "new file mode" not in revision:
+            commit_id_and_number_of_lines_affected = process_revision(parsed_revision, parent_commit_id)
+        else:
+            print "file: %s - new file" % parsed_revision[0][0]
 
         for prev_commit_id in commit_id_and_number_of_lines_affected.keys():
             if prev_commit_id in commit_origin_of_removed_lines:
@@ -135,13 +149,16 @@ def process_commit(commit_id, parent_commit_id):
     print "Commits which had lines which were removed by current commit: %s \n" % commit_origin_of_removed_lines
 
 
+
+
+
 def run_script():
 
     postgres = Postgres()
     commits = postgres.get_commits()
 
     manual_run = None
-    #manual_run = 46
+    # manual_run = 26
 
     if not manual_run:
         for index in range(50):
